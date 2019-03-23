cccfe636977d8ecd6ea35883de14c2a4c54f7a10
Muhammad Taha <taha_khan198923@yahoo.com>
2019-03-23 18:28:49 +0500

    added cordova speech recognization api

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
+interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+  SpeechRecognition: any;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,9 +19,19 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+   
+    public speech: SpeechRecognition,
   ) {
     this.initializeApp();
+  }
+
+  goAccount() {
+    this.router.navigateByUrl('/user-account');
+  }
+  goInbox() {
+    this.router.navigateByUrl('/user-account');
   }
 
   initializeApp() {
@@ -22,5 +39,22 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    
+ this.hasPermission().then(()=>{});
+   
   }
+
+  async hasPermission():Promise<boolean> {
+    try {
+      const permission = await this.speech.hasPermission();
+      console.log('permission', permission);
+
+      return permission;
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+
+
 }
