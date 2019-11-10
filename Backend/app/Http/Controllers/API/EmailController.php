735ac5abd5be364124c4702bf95e91776c78f1d6
Muhammad Taha <taha_khan198923@yahoo.com>
2019-11-11 02:02:30 +0500

    work for outbox

--- Backend/app/Http/Controllers/API/EmailController.php
+++ Backend/app/Http/Controllers/API/EmailController.php
@@ -31,6 +31,27 @@ class EmailController extends BaseController
         }
     }
 
+    public function outbox(Request $request) {
+        try {
+            
+            $endpoint = "http://127.0.0.1:5000/outbox";
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request('GET', $endpoint, ['query' => [
+                'email' => Auth::user()['email'], 
+                'password' => Auth::user()['app_password']
+            ]]);
+            
+            $statusCode = $response->getStatusCode();
+            $content = $response->getBody();
+            // print_r(json_decode($content)); die();
+           
+            return response()->json(['success' => true, 'data' => json_decode($content)], $this->successStatus);
+           
+        } catch(\Exception $e) {
+            return $this->sendError($e->getMessage(), []);
+        }
+    }
+
     public function send_email(Request $request) {
         try {
 
